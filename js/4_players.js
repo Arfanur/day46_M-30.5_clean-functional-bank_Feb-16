45a7ee164306dacf81c3978c571d8a178d7b5891
Arfanur <arfanur03@gmail.com>
2022-04-20 16:11:03 +0600

    30_5-4 Revisit id, class ...

---
+++ js/4_players.js
@@ -0,0 +1,11 @@
+const players = document.getElementsByClassName('player');
+for (const player of players) {
+    player.style.border = '1px solid red';
+    player.style.borderRadius = '15px';
+    player.style.margin = '10px';
+    player.style.padding = '10px';
+}
+function addPlayer() {
+    const playerContainer = document.getElementById('players');
+    
+}
